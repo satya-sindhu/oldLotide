b24ac357eb384dbcc3e2de04a1f7b12376b02986
satya sindhu <movva001@gmail.com>
2021-11-02 14:54:43 -0400

    modified findkey.js

---
+++ findkey.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log("✅ " + `Assertion Passed:  ${actual}  ===  ${expected}`);
+    } else {
+      console.log("🔴 " + `Assertion Failed:  ${actual}  !==  ${expected}`);
+    }
+  };
+  const findKey = (obj, cb) => {
+    let key = undefined;
+  
+    for (let item in obj) {
+      if (cb(obj[item])) {
+        return item;
+      }
+    }
+    return key;
+  };
+
+  // Test case 1
+const test1 = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 20);
+  
+  assertEqual(test1, undefined);
+  
+  // Test case 2
+  const test2 = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 3);
+  
+  assertEqual(test2, "Akaleri");
